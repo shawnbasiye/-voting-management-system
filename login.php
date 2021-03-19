5bcac470a8a86ddff3e5ab75aa23b2a0d3f89da0
shawnbasiye <shawnbasiye07@gmail.com>
2021-03-19 22:41:19 +0300

    fix

--- login.php
+++ login.php
@@ -1,23 +1,29 @@
 <?php
   require_once './includes/guestheader.php';
 ?>
-  
-   <div class="container" >
-   <div class="header">
-   <h2>Login</h2>
-   </div>
-   <form action="Registartion.php" method="post">
-      <div>
-         <label for= "Reg_No">Reg_No:</label>
-         <Input type="text" name="Reg_No"required>
-      </div>
+<!-- Login form 
+   containes the following controls:
+   - reg_no
+   -password
+ -->
 
-      <div>
-         <label for= "Password">Password:</label>
-         <Input type="text" name="Password"required>
+<div class="container" >
+      <div class="row">
+        <div class="col-sm-4 m-auto border p-2 shadow mt-3">
+           <h2 class="text-center">Login</h2>
+         
+   <form action="./controllers/login.php" method="post">
+       <div class="mb-2">
+           <label for= "reg_on">Reg_No:</label>
+           <Input type="text" class="form-control"  name="reg_no" id="reg_no" required>
+       </div>
+
+      <div class="mb-2">
+         <label for= "password">Password:</label>
+         <Input type="password" class="form-control"  name="password" id="password" required>
       </div>
    </form>
-   <button type="Submit" name = "Login_User">Login</button>
+   <input type="Submit" class="btn btn-primary" name="Login" value="Submit" />
    <p>Not a User?<a href="registration.php"><b>Register Here</b></a></p>
    </div>
 
--- registration.php
+++ registration.php
@@ -8,41 +8,43 @@
    - email
    - password
    - reg_no
-   - confirm_passwordd
+   - confirm_password
  -->
 
 
 <div class="container" >
    <div class="row">
-      <div class="col-sm-6 m-auto border p-2 shadow mt-3">
+      <div class="col-sm-4 m-auto border p-2 shadow mt-3">
          <h2 class="text-center">Register</h2>
          <form action="./controllers/register.php" method="post">
-            <div class="mb-3">
+
+          <div class="mb-2"> 
                <label for="first_name">First name:</label>
                <Input type="text" class="form-control" name="first_name" id="first_name"  required>
             </div>
 
-            <div class="mb-3">
                <label for="last_name">Last name:</label>
+            <div class="mb-2">
                <Input type="text" class="form-control"  name="last_name" id="last_name" required>
             </div>
 
-            <div class="mb-3">
                <label for="email">Email:</label>
+            <div class="mb-2">
                <Input type="email" class="form-control"  name="email"required>
             </div>
 
-            <div class="mb-3">
                   <label for="reg_no">Reg_No:</label>
+            <div class="mb-2">
                   <Input type="text" class="form-control"  name="reg_no" id="reg_no" required>
             </div>
 
-            <div class="mb-3">
+         
+            <div class="mb-2">
                   <label for="password">Password:</label>
                   <Input type="password" class="form-control"  name="password" id="password" required>
             </div>
 
-            <div class="mb-3">
+            <div class="mb-2">
                   <label for="confirm_password">Confirm Password:</label>
                   <Input type="password" name="confirm_password" class="form-control"  id="confirm_password" required>
             </div>
